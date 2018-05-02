b8439acfab3ec8322e2c837bc4cbb72750845920
DavoudTaghawiNejad <davoud@taghawi-nejad.de>
2018-05-02 15:45:19 +0100

    Add 'end of period' to booking history

--- accounting.py
+++ accounting.py
@@ -132,8 +132,8 @@ class AccountingSystem:
         else:
             debit_accounts.append((self.residual_account_name, -profit))
 
-    self.book(debit=debit_accounts, credit=credit_accounts, text='Period close')
-
+        self.book(debit=debit_accounts, credit=credit_accounts, text='Period close')
+        self.booking_history("Period close")
         for account in self.flow_accounts:
             account = Account()
 
