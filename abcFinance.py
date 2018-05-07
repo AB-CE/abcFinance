7dd40bc2f6a2a84eff90da71bd0840cec7ad2c13
7brunner <7brunner@gmx.at>
2018-05-07 18:24:27 +0100

    Autobook for abcFinance.Agent

--- abcFinance.py
+++ abcFinance.py
@@ -5,15 +5,44 @@ Created on Sun May  6 18:59:39 2018
 @author: christoph
 """
 
-from accounting import AccountingSystem,Account,s
 import abce
+from accountingsystem import AccountingSystem
+from account import Account, s
 
-class AccountingAgent(abce.Agent):
+
+class Agent(abce.Agent):
     def __init__(self, *param, **kwparam):
-        super().__init__(*param,**kwparam)
-        
-        if 'residual_account_name' in kwparam['agent_arguments']:
-            residual_account_name = kwparam['agent_arguments']['residual_account_name']
-            self.accounts = AccountingSystem(residual_account_name = residual_account_name)
+        super().__init__(*param, **kwparam)
+
+        if 'residual_account_name' in kwparam:
+            self.accounts = AccountingSystem(residual_account_name=
+                                             kwparam['residual_account_name'])
         else:
             self.accounts = AccountingSystem()
+
+    def book_end_of_period(self):
+        self.accounts.book_end_of_period()
+
+    def book(self, debit, credit, text=''):
+        self.accounts.book(debit, credit, text)
+
+    def make_stock_accounts(self, names):
+        self.accounts.make_stock_accounts(names)
+
+    def make_flow_accounts(self, names):
+        self.accounts.make_flow_accounts(names)
+
+    def make_asset_accounts(self, names):
+        self.accounts.make_stock_accounts(names)
+
+    def make_liability_accounts(self, names):
+        self.accounts.make_liability_accounts(names)
+
+    def print_profit_and_loss(self, show_empty_accounts=False):
+        self.accounts.print_profit_and_loss(show_empty_accounts)
+
+    def print_balance_sheet(self, show_empty_accounts=False):
+        self.accounts.print_balance_sheet(show_empty_accounts)
+
+    def _autobook(self, msg):
+        self.accounts.book(**msg.content)
