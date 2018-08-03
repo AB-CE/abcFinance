be7ae3d495560bc9b727f70154217279b90bed52
DavoudTaghawiNejad <davoud@taghawi-nejad.de>
2018-08-03 11:39:25 +0100

    Use abcEconomics instead of abce in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup
 
 
+install_requires = ['abcEconomics']
+
 setup(name='abcFinance',
       version='0.1',
       description='',
       url='https://github.com/AB-CE/abcFinance',
       packages=['abcFinance'],
-      zip_safe=True)
+      zip_safe=True,
+      install_requires=install_requires)
