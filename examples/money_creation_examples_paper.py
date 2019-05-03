5136c4fc5537013eea9671a5e84ae606a9c8015d
siebenbrunner <christoph@siebenbrunner.com>
2019-05-04 00:41:00 +0200

    Update money creation example

--- examples/money_creation_examples_paper.py
+++ examples/money_creation_examples_paper.py
@@ -9,41 +9,41 @@ central_bank = Ledger(residual_account_name="Equity")
 government = Ledger(residual_account_name="Equity")
 
 bank.make_stock_accounts(['Currency','Loans','Reserves','Bonds','Deposits','Government Deposits','Wholesale Deposits'])
-private_agent.make_stock_accounts(['Currency','Deposits','Loans'])
+private_agent.make_stock_accounts(['Currency','Deposits','Loans','Bonds'])
 central_bank.make_stock_accounts(['Assets','Reserves','Government Reserves','Currency'])
 government.make_stock_accounts(['Deposits','Bonds','Reserves'])
 
 """ ---------------------------------------------------------------------------
 Initialize agents' balance sheets
 """
-private_agent.book(debit=[('Currency',100)],credit=[('Equity',100)],text='(1)')
-bank.book(debit=[('Currency',50)],credit=[('Equity',50)],text='(1)')
-central_bank.book(debit=[('Assets',150)],credit=[('Currency',150)],text='(1)')
+private_agent.book(debit=[('Currency',500)],credit=[('Equity',500)])
+bank.book(debit=[('Currency',50)],credit=[('Equity',50)])
+central_bank.book(debit=[('Assets',550)],credit=[('Currency',550)])
 
-private_agent.book(debit=[('Deposits',100)],credit=[('Currency',100)],text='(2)')
-bank.book(debit=[('Currency',100)],credit=[('Deposits',100)],text='(2)')
+private_agent.book(debit=[('Deposits',500)],credit=[('Currency',500)])
+bank.book(debit=[('Currency',500)],credit=[('Deposits',500)])
 
-bank.book(debit=[('Reserves',150)],credit=[('Currency',150)],text='(3)')
-central_bank.book(debit=[('Currency',150)],credit=[('Reserves',150)],text='(2)')
-
-""" ---------------------------------------------------------------------------
-Take out a loan
-"""
-private_agent.book(debit=[('Deposits',850)],credit=[('Loans',850)],text='(3)')
-bank.book(debit=[('Loans',850)],credit=[('Deposits',850)],text='(4)')
+bank.book(debit=[('Reserves',550)],credit=[('Currency',550)])
+central_bank.book(debit=[('Currency',550)],credit=[('Reserves',550)])
 
 """ ---------------------------------------------------------------------------
 Bank buys a government bond
 """
-government.book(debit=[('Reserves',100)],credit=[('Bonds',100)],text='(1)')
-bank.book(debit=[('Bonds',100)],credit=[('Reserves',100)],text='(5)')
-government.book(debit=[('Deposits',100)],credit=[('Reserves',100)],text='(1)')
-bank.book(debit=[('Reserves',100)],credit=[('Deposits',100)],text='(5)')
+government.book(debit=[('Reserves',400)],credit=[('Bonds',400)])
+bank.book(debit=[('Bonds',400)],credit=[('Reserves',400)])
+central_bank.book(debit=[('Reserves',400)],credit=[('Government Reserves',400)])
 
-government.book(debit=[('Deposits',100)],credit=[('Bonds',100)],text='(1)')
-bank.book(debit=[('Bonds',100)],credit=[('Deposits',100)],text='(5)')
+""" ---------------------------------------------------------------------------
+Private agent buys the bond from the bank
+"""
+private_agent.book(debit=[('Bonds',100)],credit=[('Deposits',100)])
+bank.book(debit=[('Deposits',100)],credit=[('Bonds',100)])
 
-
+""" ---------------------------------------------------------------------------
+Take out a loan
+"""
+private_agent.book(debit=[('Deposits',550)],credit=[('Loans',550)])
+bank.book(debit=[('Loans',550)],credit=[('Deposits',550)])
 
 private_agent.print_balance_sheet()
 bank.print_balance_sheet()
